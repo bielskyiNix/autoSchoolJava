5e7d3961d453ec431c4c3ae9aa793712676d77a4
bielskyi <denis.bielskyi@nixsolutions.com>
2020-08-31 14:18:21 +0300

    Changes for practical tasks of control constructs part (module 1)

--- src/java/com/module_1/control_constructs/Main.java
+++ src/java/com/module_1/control_constructs/Main.java
@@ -6,16 +6,21 @@ public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
+        System.out.println("Practical tasks 1 - 7");
         ConditionalStatementsAndSwith.twoNumbers();
         ConditionalStatementsAndSwith.fourNumbers();
         ConditionalStatementsAndSwith.threeNumbers();
         ConditionalStatementsAndSwith.Names();
         ConditionalStatementsAndSwith.nameAndAge();
         ConditionalStatementsAndSwith.gamewithNumbers();
+
+        System.out.println("Practical tasks 8 - 12");
         WhileCycle.while10("D"); // A = Ascending, D = Descending
         WhileCycle.stringXNumber();
         WhileCycle.letterCube();
         WhileCycle.multiplicationTable();
+
+        System.out.println("Practical tasks 13 - 18");
         ForCycle.for10();
         ForCycle.rectangle();
         ForCycle.triangle();
