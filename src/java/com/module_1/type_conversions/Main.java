64b295d2f1fc365dd75a078650bf3f10ac5565e0
bielskyi <denis.bielskyi@nixsolutions.com>
2020-08-31 16:09:43 +0300

    Module 1 Type conversions changes

--- src/java/com/module_1/type_conversions/Main.java
+++ src/java/com/module_1/type_conversions/Main.java
@@ -3,26 +3,34 @@ package com.module_1.type_conversions;
 public class Main {
 
     public static void main(String[] args) {
+
         practicalTask1(30,14);
         practicalTask2(77);
         practicalTask3(5.9);
         practicalTask4(10,15,47);
+
     }
 
     public static void practicalTask1(int q,int w){
+
         int devisionResult = q / w;
         int remainderFromDivision = q % w;
         System.out.println("The result from division: " + devisionResult + " and remainder from division: " + remainderFromDivision );
+
     }
 
-    public static void practicalTask2(int n){
-        int k = (int)n/10 + (n - ((int)n/10)*10);
+    public static void practicalTask2(int n) {
+
+        int k = (int) n / 10 + ( n - ( (int) n / 10 ) * 10);
         System.out.println("Sum " + k);
+
     }
 
-    public static void practicalTask3(double n){
+    public static void practicalTask3(double n) {
+
         int round = (int) (n + 0.5);
         System.out.println("Rounded number " + round);
+
     }
 
 
