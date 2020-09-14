177470a0d8b6f9113bffb1d796891849f4296ab4
bielskyi <denis.bielskyi@nixsolutions.com>
2020-09-14 11:18:46 +0300

    Module 4 finished

--- src/main/java/com/module_4/reflection_api/Program.java
+++ src/main/java/com/module_4/reflection_api/Program.java
@@ -1,6 +1,7 @@
 package com.module_4.reflection_api;
 
 import java.lang.reflect.Field;
+import java.util.Random;
 
 
 public class Program {
@@ -11,9 +12,26 @@ public class Program {
         Field[] field = object.getClass().getDeclaredFields();
         for(int i = 0; i < field.length; i++ ){
             field[i].setAccessible(true);
-            field[i].set(object,"ObjectName "+i);
+            field[i].set(object,generateRandomString(10));
         }
 
+    }
+
+    public static String generateRandomString(int stringLength) {
+
+        // elements from 0 to z (0-9, A-Z, a-z and :,;,<,=,>,?,@,[,\,],^,_,`)
+        // 48-57 = 0-9 , 58-64 = :,;,<,=,>,?,@ , 65-90 = A-Z , 91-96 [,\,],^,_,` , 97-122 = a-z
+        int leftLimit = 48; // number '0'
+        int rightLimit = 122; // letter 'z'
+        Random random = new Random();
+
+        // буду честен нашел вариант генерации рандомной строки в инете) и немного отредактировал) и вроде разобрался в чем суть)
+        String generatedString = random.ints(stringLength, leftLimit, rightLimit + 1)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+
+
+        return generatedString;
     }
 
 }
